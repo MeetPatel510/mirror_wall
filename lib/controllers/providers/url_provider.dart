d961b975036a05741fd82f22497eba229df20522
Meet <meetpatel51099@gmail.com>
2023-09-18 16:15:26 +0530

    complete

--- lib/controllers/providers/url_provider.dart
+++ lib/controllers/providers/url_provider.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../../models/url_model.dart';
 
 class UrlProvider extends ChangeNotifier {
